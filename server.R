29753e65117dfd9f4b80b5a78fd20d9580fe5d7b
Francesco Strozzi <francesco.strozzi@gmail.com>
2016-06-14 14:02:45 +0900

    extracting biocyc data

--- server.R
+++ server.R
@@ -9,6 +9,7 @@ library(shiny)
 library(DT)
 library(SPARQL)
 library(GetoptLong)
+library(data.table)
 
 protein_to_pathway <- function(id){
   # Step 1 - Set up preliminaries and define query
@@ -17,22 +18,21 @@ protein_to_pathway <- function(id){
   
   # create query statement
   query <-qq("
-             PREFIX up:<http://purl.uniprot.org/core/> 
-             PREFIX keywords:<http://purl.uniprot.org/keywords/> 
-             PREFIX uniprotkb:<http://purl.uniprot.org/uniprot/> 
-             PREFIX taxon:<http://purl.uniprot.org/taxonomy/> 
-             PREFIX ec:<http://purl.uniprot.org/enzyme/> 
-             PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
-             PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#> 
-             PREFIX skos:<http://www.w3.org/2004/02/skos/core#> 
-             PREFIX owl:<http://www.w3.org/2002/07/owl#> 
-             PREFIX bibo:<http://purl.org/ontology/bibo/> 
-             PREFIX dc:<http://purl.org/dc/terms/> 
-             PREFIX xsd:<http://www.w3.org/2001/XMLSchema#> 
-             PREFIX faldo:<http://biohackathon.org/resource/faldo#> 
+             PREFIX up:<http://purl.uniprot.org/core/>
+             PREFIX keywords:<http://purl.uniprot.org/keywords/>
+             PREFIX uniprotkb:<http://purl.uniprot.org/uniprot/>
+             PREFIX taxon:<http://purl.uniprot.org/taxonomy/>
+             PREFIX ec:<http://purl.uniprot.org/enzyme/>
+             PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+             PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#>
+             PREFIX skos:<http://www.w3.org/2004/02/skos/core#>
+             PREFIX owl:<http://www.w3.org/2002/07/owl#>
+             PREFIX bibo:<http://purl.org/ontology/bibo/>
+             PREFIX dc:<http://purl.org/dc/terms/>
+             PREFIX xsd:<http://www.w3.org/2001/XMLSchema#>
+             PREFIX faldo:<http://biohackathon.org/resource/faldo#>
              
-             
-             SELECT ?ko ?unipathway ?pathwayName
+             SELECT ?ko ?unipathway ?pathwayName ?biocyc
              WHERE
              {
              uniprotkb:@{id} rdfs:seeAlso ?ko .
@@ -41,7 +41,10 @@ protein_to_pathway <- function(id){
              ?node rdf:type up:Pathway_Annotation .
              ?node rdfs:seeAlso ?unipathway .
              ?unipathway rdfs:label ?pathwayName .
-             }")
+             uniprotkb:@{id} rdfs:seeAlso ?biocyc . 
+             ?biocyc up:database <http://purl.uniprot.org/database/BioCyc>
+             }
+             ")
 
   # Step 2 - Use SPARQL package to submit query and save results to a data frame
   qd <- SPARQL(endpoint,query)
@@ -49,21 +52,27 @@ protein_to_pathway <- function(id){
   df
 }
 
-create_url = function(x) {
+uri2url = function(x) {
   x = gsub('<','',x)
   x = gsub('>','',x)
   name = tail(unlist(strsplit(x,"/")),n=1)
-  return(toString(tags$a(href=x,name)))
+  toString(tags$a(href=x,name))
+}
+
+create_urls = function(dt) {
+  dt[,ko:=uri2url(ko), by = 1:nrow(dt)]
+  dt[,unipathway:=uri2url(unipathway), by = 1:nrow(dt)]
+  dt[,biocyc:=uri2url(biocyc), by = 1:nrow(dt)]
+  dt
 }
 
 
 shinyServer(function(input, output) {
-
   output$table = DT::renderDataTable({
     print(input$protein)
-    df = protein_to_pathway(input$protein)
-    df[c("ko","unipathway")] = lapply(dt[c("ko","unipathway")], function(x) {create_url(x)})
-    datatable(df,escape = F)
-  } )
+    dt = as.data.table(protein_to_pathway(input$protein))
+    dt = create_urls(dt)
+    datatable(dt,escape = F)
+  })
 
 })
